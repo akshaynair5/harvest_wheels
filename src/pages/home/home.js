004ff47f53d6cd1e9e3faaaac20af14074e6cc39
Akshay Mathilakth Nair <aks7aynair@gmail.com>
2024-03-11 23:25:38 +0530

    changes

--- src/pages/home/home.js
+++ src/pages/home/home.js
@@ -1,31 +1,92 @@
 import './home.scss'
 import Sidebar from '../../components/sidebar/sidebar'
+import linkLine from '../../images/linkLine.png'
+import bg from '../../images/bg.jpeg'
 
 function Home(){
     const obj =  [{
-        userId:'b4oxnn4Gv7XTUuTB4s8i7ui7h3G2"',
-        profileURL:'https://firebasestorage.googleapis.com/v0/b/harvest-wheels.appspot.com/o/1709743834705?alt=media&token=8c67be27-a5f4-41e0-b790-afe8984393bd',
-        name:'Akshay',
-        type:'request',
-        start:'pune',
-        destination:'thane',
-        date:'16/08/2024',
-        details:'I have 2m^2 of space left in my vehicle',
-        expiry:false,
-        reply:{
-            type:[
-                {
-                    replyUserId:"b4oxnn4Gv7XTUuTB4s8i7ui7h3G2",
-                    replyMessage:'Intrested'
-                }
-            ]
+            userId:'b4oxnn4Gv7XTUuTB4s8i7ui7h3G2"',
+            profileURL:'https://firebasestorage.googleapis.com/v0/b/harvest-wheels.appspot.com/o/1709743834705?alt=media&token=8c67be27-a5f4-41e0-b790-afe8984393bd',
+            name:'Akshay',
+            type:'request',
+            start:'pune',
+            destination:'thane',
+            date:'16/08/2024',
+            details:'I have 2m^2 of space left in my vehicle',
+            expiry:false,
+            reply:{
+                type:[
+                    {
+                        replyUserId:"b4oxnn4Gv7XTUuTB4s8i7ui7h3G2",
+                        replyMessage:'Intrested'
+                    }
+                ]
+            },
+        },
+        {
+            userId:'b4oxnn4Gv7XTUuTB4s8i7ui7h3G2"',
+            profileURL:'https://firebasestorage.googleapis.com/v0/b/harvest-wheels.appspot.com/o/1709743834705?alt=media&token=8c67be27-a5f4-41e0-b790-afe8984393bd',
+            name:'Akshay',
+            type:'request',
+            start:'pune',
+            destination:'thane',
+            date:'16/08/2024',
+            details:'I have 2m^2 of space left in my vehicle',
+            expiry:false,
+            reply:{
+                type:[
+                    {
+                        replyUserId:"b4oxnn4Gv7XTUuTB4s8i7ui7h3G2",
+                        replyMessage:'Intrested'
+                    }
+                ]
+            }
+        },
+        {
+            userId:'b4oxnn4Gv7XTUuTB4s8i7ui7h3G2"',
+            profileURL:'https://firebasestorage.googleapis.com/v0/b/harvest-wheels.appspot.com/o/1709743834705?alt=media&token=8c67be27-a5f4-41e0-b790-afe8984393bd',
+            name:'Akshay',
+            type:'request',
+            start:'pune',
+            destination:'thane',
+            date:'16/08/2024',
+            details:'I have 2m^2 of space left in my vehicle',
+            expiry:false,
+            reply:{
+                type:[
+                    {
+                        replyUserId:"b4oxnn4Gv7XTUuTB4s8i7ui7h3G2",
+                        replyMessage:'Intrested'
+                    }
+                ]
+            }
+        },
+        {
+            userId:'b4oxnn4Gv7XTUuTB4s8i7ui7h3G2"',
+            profileURL:'https://firebasestorage.googleapis.com/v0/b/harvest-wheels.appspot.com/o/1709743834705?alt=media&token=8c67be27-a5f4-41e0-b790-afe8984393bd',
+            name:'Akshay',
+            type:'request',
+            start:'pune',
+            destination:'thane',
+            date:'16/08/2024',
+            details:'I have 2m^2 of space left in my vehicle',
+            expiry:false,
+            reply:{
+                type:[
+                    {
+                        replyUserId:"b4oxnn4Gv7XTUuTB4s8i7ui7h3G2",
+                        replyMessage:'Intrested'
+                    }
+                ]
+            }
         }
-
-    }]
+    
+    ]
     return(
         <div className='Home'>
             <Sidebar/>
             <div className='Main'>
+                <img src={bg} className='bg'></img>
                 <div className='loadLinks'>
                     {
                         obj.map((loadLink)=>{
@@ -36,11 +97,14 @@ function Home(){
                                             <img src={loadLink.profileURL}></img>
                                             <p className='name'>{loadLink.name}</p>
                                         </div>
-                                        <p>date {loadLink.date}</p>
-                                        <p>details {loadLink.details}</p>
+                                        <div className='desc'>
+                                            <p className='details'>{loadLink.details}</p>
+                                            <p className='date'><b>Date: {loadLink.date}</b></p>
+                                        </div>
                                         <div className='travelDetails'>
-                                            <p className='start'>Start: {loadLink.start}</p>
-                                            <p className='destination'>destination: {loadLink.destination}</p>
+                                            <p className='start'>○   Start: <b>{loadLink.start}</b></p>
+                                            <img src={linkLine}></img>
+                                            <p className='destination'>○   Destination: <b>{loadLink.destination}</b></p>
                                         </div>
                                     </div>
                                 )
@@ -51,7 +115,7 @@ function Home(){
                                         <p>Start: {loadLink.start}</p>
                                         <p>destination: {loadLink.destination}</p>
                                         <p>date {loadLink.date}</p>
-                                        <p>details {loadLink.details}</p>
+                                        <p>Details {loadLink.details}</p>
                                     </div>
                                 )
                             }
